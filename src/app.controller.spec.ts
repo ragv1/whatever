5b2712d374bab10822b1ecf5f93ab67c4b230987
ragv1 <ramonambiorix14@gmail.com>
2023-11-21 22:08:24 -0400

    init

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Bienvenidos a mi mundo"', () => {
+      expect(appController.getHello()).toBe('Bienvenidos a mi mundo');
     });
   });
 });
